1a01fa64e7a835d469e556d89ef206a8c35b8397
Tunn2 <ktung7574@gmail.com>
2024-01-27 14:08:17 +0700

    lan thu 2 lam chuyen ay

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -9,7 +9,10 @@ public class MathUtilLive {
 //        MathUtil.getFactorial(-5);
         System.out.println("expected: 6! = 720; actual: " + MathUtil.getFactorial(6));
         System.out.println("expected: 0! = 1; actual: " + MathUtil.getFactorial(0));
-
+        
+        //them code sau lan dau tien lam chuyen ay - code len server 2:07pm
+        System.out.println("expected: 1! = 1; actual: " + MathUtil.getFactorial(1));
+        System.out.println("expected: 3! = 6; actual: " + MathUtil.getFactorial(3));
     }
 
 }
